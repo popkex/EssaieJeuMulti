14d8ce5ca1f77bf57e545427529fe88228ab020e
popkex <clementbertrand52@gmail.com>
2024-10-31 22:39:02 +0100

    amélioration des commentaires

--- client.py
+++ client.py
@@ -37,27 +37,38 @@ class Client(threading.Thread):
             print(f"Connexion au serveur échouée ! Erreur: {e}")
             self.socket.close()  # Fermer le socket en cas d'échec de connexion
 
+
+    def format_data_without_content(self, order_code):
+        """Format et envoie les codes au server"""
+        data = _pcs.codes[order_code].encode('utf8')
+        self.socket.send(data)  # Utiliser send au lieu de sendto
+
+    def format_data_with_content(self, order_code, content):
+        """Format et envoie les codes et les données au server"""
+        if order_code == "PositionPlayer":
+            # Vérification si data_content est un tuple de deux floats
+            if isinstance(content, tuple) and len(content) == 2:
+                # Préparer la structure de données à encoder
+                data_to_send = f"{_pcs.codes[order_code][0]}|{content}"
+                data = data_to_send.encode('utf8')
+
+                self.socket.send(data)
+                # Ici, nous n'encode pas encore, juste une préparation
+            else:
+                print(f"\033[31mLe type de donnée fournie n'est pas correcte,\033[34m type(data_content): {type(content)}\033[0m")
+
     def send_order(self, order_code, data_content=None):
+        """Logique de formatage et d'envoie des données"""
         if order_code in _pcs.codes:
             # si aucune donnée n'est donnée, considerer qu'il n'y en a pas a donner
             if not data_content:
-                data = _pcs.codes[order_code].encode('utf8')
-                self.socket.send(data)  # Utiliser send au lieu de sendto
+                self.format_data_without_content(order_code)
             else:
-                if order_code == "PositionPlayer":
-                    # Vérification si data_content est un tuple de deux floats
-                    if isinstance(data_content, tuple) and len(data_content) == 2:
-                        # Préparer la structure de données à encoder
-                        data_to_send = f"{_pcs.codes[order_code][0]}|{data_content}"
-                        data = data_to_send.encode('utf8')
+                self.format_data_with_content(order_code, data_content)
 
-                        self.socket.send(data)
-                        # Ici, nous n'encode pas encore, juste une préparation
-                    else:
-                        print(f"\033[31mLe type de donnée fournie n'est pas correcte,\033[34m type(data_content): {type(data_content)}\033[0m")
 
     def execute_order(self, data_received):
-        """extrès le code et les données puis execute l'ordre adequate"""
+        """Extrès le code et les données puis execute l'ordre adequate"""  # oui je sais pas écrire mais oklm demande a chatgpt
         order_code, data_content_str = data_received.split(", ", 1)  # Séparer le code du reste
 
         if order_code == _pcs.codes["PositionPlayer"][0] and data_content_str:
@@ -69,6 +80,7 @@ class Client(threading.Thread):
                 ip = ip_port[0]
                 result = [ip, coords]  # Créer la structure finale dans le format voulu
 
+
     def disconnect(self):
         self.send_order("PlayerDisconnect")
         self.socket.close()  # Fermer le socket
@@ -76,17 +88,23 @@ class Client(threading.Thread):
     def connect(self):
         self.send_order("PlayerConnect")
 
+
     def send_update(self):
+        """Envoie l'ensemble des données utiles a actualiser le jeu"""
         player_position = self.game.player.position
         self.send_order("PositionPlayer", player_position)
 
+
     def get_order(self):
+        """Recupère toutes les données envoyers"""
         data = self.socket.recv(1024).decode('utf8')  # Recevoir des données du serveur
 
         if data:
             self.execute_order(data)  # Traiter les données reçues
 
+
     def run(self):
+        """Boucle d'actualisation"""
         self.connect()
 
         while self.is_connected:
