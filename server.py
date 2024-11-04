7887f95d3f06ae64ea764f6d05af2880d75c9b35
popkex <clementbertrand52@gmail.com>
2024-11-04 20:36:32 +0000

    modification du rafraichissement cdu server

--- server.py
+++ server.py
@@ -143,8 +143,7 @@ class GameDataSender(threading.Thread):
     def regroup_data(self):
         """Envoie les données importantes aux clients"""
         while True:
-            time.sleep(0.1)  # evite la surcharge
-
+            time.sleep(0.015)
             code_and_players_pos = f"PPos, {data_base.player_pos}"
             self.server.send_data_to_clients(code_and_players_pos)
 
