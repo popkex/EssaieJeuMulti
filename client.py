81c8db310ccd8453b95e425a59546e723abcf826
popkex <clementbertrand52@gmail.com>
2024-11-12 12:34:35 +0100

    changement TCP et UDP

--- client.py
+++ client.py
@@ -13,11 +13,9 @@ host, port = ('89.168.57.22', 49352)
 
 @dataclass
 class DataBase:
-    player_pos: Dict[Tuple[str, int], Tuple[float, float]] = field(default_factory=dict)  # Format : {(ip, port): (x, y)}
-
+    player_pos: Dict[Tuple[str, int], Tuple[float, float]] = field(default_factory=dict)
 
 data_base = DataBase()
-
 
 class Client(threading.Thread):
     def __init__(self, game):
@@ -27,54 +25,42 @@ class Client(threading.Thread):
         self.clock = pygame.time.Clock()
 
         print("Lancement de la connexion au serveur...")
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Créer le socket
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-        self.socket.settimeout(0.5)  # ajoute un timeout pour eviter le bloquage
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Utiliser UDP
+        self.socket.settimeout(0.5)
 
-        try:
-            self.socket.connect((host, port))  # Connecter le socket
-            print("Client connecté !")
-        except Exception as e:
-            print(f"Connexion au serveur échouée ! Erreur: {e}")
-            self.socket.close()  # Fermer le socket en cas d'échec de connexion
-
+        # Pas de connexion explicite pour UDP
+        print("Client prêt à envoyer et recevoir des messages UDP.")
 
     def get_players_position(self):
-        return list(data_base.player_pos.items())  # list() pour transformer le dic en list // .items() pour recupere a la fois la clé mais aussi la donnée
+        return list(data_base.player_pos.items())
 
     def get_my_id(self):
-        """Recupère l'ip public et le port du client"""
-        ip = requests.get("https://api.ipify.org?format=text").text  # Récupère l'IP publique
-        port = self.socket.getsockname()[1]  # Récupère le port local utilisé pour communiquer avec le serveur
+        """Récupère l'IP publique et le port local du client"""
+        ip = requests.get("https://api.ipify.org?format=text").text
+        port = self.socket.getsockname()[1]
         return (ip, port)
 
     def get_socket(self):
         return self.socket
 
-
-
     def format_data_without_content(self, order_code):
-        """Format et envoie les codes au server"""
+        """Format et envoie les codes au serveur"""
         data = _pcs.codes[order_code].encode('utf8')
-        self.socket.send(data)  # Utiliser send au lieu de sendto
+        self.socket.sendto(data, (host, port))
 
     def format_data_with_content(self, order_code, content):
-        """Format et envoie les codes et les données au server"""
+        """Format et envoie les codes et les données au serveur"""
         if order_code == "PositionPlayer":
-            # Vérification si data_content est un tuple de deux floats
             if isinstance(content, tuple) and len(content) == 2:
-                # Préparer la structure de données à encoder
                 data_to_send = f"{_pcs.codes[order_code][0]}|{content}"
                 data = data_to_send.encode('utf8')
-
-                self.socket.send(data)
+                self.socket.sendto(data, (host, port))
             else:
-                print(f"\033[31mLe type de donnée fournie n'est pas correcte,\033[34m type(data_content): {type(content)}\033[0m")
+                print(f"\033[31mLe type de donnée fourni n'est pas correct,\033[34m type(content): {type(content)}\033[0m")
 
     def send_order(self, order_code, data_content=None):
-        """Logique de formatage et d'envoie des données"""
+        """Logique de formatage et d'envoi des données"""
         if order_code in _pcs.codes:
-            # si aucune donnée n'est donnée, considerer qu'il n'y en a pas a donner
             if not data_content:
                 self.format_data_without_content(order_code)
             else:
@@ -82,68 +68,63 @@ class Client(threading.Thread):
 
     def execute_order(self, data_received):
         """Extrait le code et les données puis exécute l'ordre adéquat"""
-
-        # Utilisation d'une expression régulière pour capturer uniquement la première occurrence
-        order_code, data_content_str = data_received.split(", ", 1)  # Séparer le code du reste
+        order_code, data_content_str = data_received.split(", ", 1)
 
         match = re.search(r"(\{.+?\})", data_content_str)
         
         if match:
-            # Extraire l'expression correspondant aux données
             data_content_str = match.group(1)
 
             try:
-                # Convertir la chaîne en dictionnaire
                 data_content = ast.literal_eval(data_content_str)
             except (SyntaxError, ValueError) as e:
                 print(f"Erreur de parsing des données: {e}")
                 return
 
-            # Vérifier si le dictionnaire est vide
-            if data_content:  # Si data_content n'est pas vide, on continue
-                data_content = list(data_content.items())  # transforme data_content en liste pour pouvoir travailler dessus plus facilement
+            if data_content:
+                data_content = list(data_content.items())
 
                 if order_code == _pcs.codes["PositionPlayer"][0]:
-                    data_base.player_pos.clear()  # reset la position de tous les joueurs pour supprimer ceux qui ne sont plus connectés
+                    data_base.player_pos.clear()
 
                     for data in data_content:
-                        ip_port, coords = data  # Extraire l'IP et les coordonnées
-                        data_base.player_pos[ip_port] = coords  # Met à jour dans la base de données
+                        ip_port, coords = data
+                        data_base.player_pos[ip_port] = coords
         else:
             print("Erreur : aucune donnée 'PPos, { ... }' trouvée dans data_received")
 
     def disconnect(self):
         self.send_order("PlayerDisconnect")
-        self.socket.close()  # Fermer le socket
+        self.socket.close()
 
     def connect(self):
         self.send_order("PlayerConnect")
 
-
     def send_update(self):
-        """Envoie l'ensemble des données utiles a actualiser le jeu"""
+        """Envoie l'ensemble des données utiles pour actualiser le jeu"""
         self.send_player_position()
 
     def send_player_position(self, position=None, send=True):
         """Envoie la position du joueur"""
-        if position:
-            player_position = position  # Si une position est fournie, on la utilise
-        else:
-            player_position = self.game.player.position
+        player_position = position if position else self.game.player.position
 
         if send:
             self.send_order("PositionPlayer", player_position)
         else:
             return player_position
 
-
     def get_order(self):
-        """Recupère toutes les données envoyers"""
-        data = self.socket.recv(1024).decode('utf8')  # Recevoir des données du serveur
+        """Récupère toutes les données envoyées"""
+        try:
+            data, addr = self.socket.recvfrom(1024)
+            data = data.decode('utf8')
 
-        if data:
-            self.execute_order(data)  # Traiter les données reçues
-
+            if data:
+                self.execute_order(data)
+        except socket.timeout:
+            pass
+        except Exception as e:
+            print(f"Erreur dans get_order(): {e}")
 
     def run(self):
         """Boucle d'actualisation"""
@@ -152,8 +133,6 @@ class Client(threading.Thread):
         while self.is_connected:
             try:
                 self.get_order()
-            except socket.timeout:
-                pass
             except Exception as e:
                 print(f"Erreur dans get_order(): {e}")
                 self.socket.close()
@@ -162,6 +141,6 @@ class Client(threading.Thread):
             try:
                 self.send_update()
             except Exception as e:
-                print(f"Echec de l'envoie des données : {e}")
+                print(f"Echec de l'envoi des données : {e}")
 
             self.clock.tick(60)
--- server.py
+++ server.py
@@ -1,11 +1,5 @@
 # coding:utf-8
 
-"""
-    TODO:
-        faire les collisions ici
-        mettre en bleu clair les nouvelles connection et en bleufoncer les deconnections
-"""
-
 import socket
 import threading
 import time
@@ -13,36 +7,30 @@ import protocolClientServer as _pcs
 from dataclasses import dataclass, field
 from typing import List, Dict, Tuple
 
-
 #----------------------------------------------------------------
 @dataclass
 class DataBase:
-    """les données du server"""
+    """les données du serveur"""
     host: str = '0.0.0.0'
     port: int = 49352
-    clients_id: List[str] = field(default_factory=list)
+    clients: List[Tuple[str, int]] = field(default_factory=list)
 
     # Informations des joueurs
     player_name: Dict[str, str] = field(default_factory=dict)  # Format : {ip: name}
-    player_ip: Dict[str, str] = field(default_factory=dict)    # Format : {name: ip}
     player_pos: Dict[Tuple[str, int], Tuple[float, float]] = field(default_factory=dict)  # Format : {(ip, port): (x, y)}
-
 
 data_base = DataBase()
 
-
-# lance le server et recupère toute les données qu'on lui envoie
 class Server:
 
     def __init__(self):
         host = data_base.host
         port = data_base.port
-        self.clients_id = data_base.clients_id
 
-        self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)  # créer le socket
-        self.socket.bind((host, port))  # s'associe à l'adresse et le port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Utiliser UDP
+        self.socket.bind((host, port))  # Associer à l'adresse et au port
 
-        # Obtenir l'adresse IP du server
+        # Obtenir l'adresse IP du serveur
         hostname = socket.gethostname()
         hostname = socket.gethostbyname(hostname)
 
@@ -52,47 +40,39 @@ class Server:
         self.game_data_sender.start()
 
     def listen(self):
-        """Écoute et accepte les connexions des clients."""
+        """Écoute et traite les messages des clients."""
         while True:
-            self.socket.listen()  # Met en écoute le serveur
-            conn, address = self.socket.accept()  # Accepte les connexions
-            print(f"Information reçue par un client {address}")
+            try:
+                data, address = self.socket.recvfrom(1024)  # Réception des données
+                data = data.decode('utf-8')
+                print(f"Message reçu de {address}: {data}")
 
-            client_thread = ThreadForClient(self, conn, address)
-            client_thread.start()
+                client_thread = ThreadForClient(self, address, data)
+                client_thread.start()
+            except Exception as e:
+                print(f"Erreur lors de la réception des données : {e}")
 
     def send_data_to_clients(self, data):
-        """envoyer les données a un client spécifique (avec sa connection)"""
+        """Envoie les données à tous les clients."""
         data = data.encode("utf-8")
 
-        for client_conn in self.clients_id:
+        for client_address in data_base.clients:
             try:
-                client_conn.send(data)  # envoie les données via chaque connexion de client
-            except BrokenPipeError as e:
-                client_conn.close()
-
-
+                self.socket.sendto(data, client_address)
+            except Exception as e:
+                print(f"Erreur lors de l'envoi des données à {client_address}: {e}")
 
 class ThreadForClient(threading.Thread):
 
-    def __init__(self, server, conn, address):
+    def __init__(self, server, address, data):
         super().__init__()
         self.server = server
-        self.conn = conn
         self.address = address
+        self.data = data
 
     def run(self):
         """Gère la communication avec le client."""
-        while True:
-            try:
-                data = self.conn.recv(1024)  # reçoit les données
-                if data:
-                    data = data.decode('utf8')
-                    self.execute_order(data)
-            except Exception as e:
-                break
-
-        self.remove_client()  # Supprime le client lorsque la communication est interrompue
+        self.execute_order(self.data)
 
     def execute_order(self, data):
         """Exécute les différentes commandes en fonction des données reçues."""
@@ -102,74 +82,43 @@ class ThreadForClient(threading.Thread):
         elif data == _pcs.codes["PlayerConnect"]:
             self.register_client()
         else:
-            """ici les ordres contiennent des données"""
-            order_code, content_string = data.split('|')  # séprart l'ordre et les données
+            order_code, content_string = data.split('|')
 
             if order_code == _pcs.codes["PositionPlayer"][0]:  # "PPos"
-                """recupère la position et l'enregistre"""
                 position_string = content_string.strip('()')
                 position = tuple(map(float, position_string.split(',')))
-                """
-                """
-                """
-                """
-                """
-                """
-                """
-                """
-                """
-                """
-                """
-                """
-                """
-                vérifier ici les collisions; définir les collisions dans le database
-                """
-                """
-                """
-                """
-                """
-                """
-                """
-                """
-                """
-                """
-                """
-                """
-                """
-                data_base.player_pos[self.address] = position  # enregistre la position du joueur
+
+                # Enregistrer la position du joueur
+                data_base.player_pos[self.address] = position
             else:
                 print("\033[31m" + f"L'ordre reçu n'est pas géré: {order_code}" + "\033[0m")
 
     def register_client(self):
         """Enregistre le client si ce n'est pas déjà fait."""
-        if self.conn not in self.server.clients_id:
-            self.server.clients_id.append(self.conn)  # Ajouter la connexion
+        if self.address not in data_base.clients:
+            data_base.clients.append(self.address)
             print(f"Client {self.address} connecté.")
 
     def remove_client(self):
-        """Supprime un client"""
-        if self.conn in self.server.clients_id:
-            self.server.clients_id.remove(self.conn)  # Retirer la connexion
-            self.conn.close()  # Fermer la connexion proprement
+        """Supprime un client."""
+        if self.address in data_base.clients:
+            data_base.clients.remove(self.address)
             data_base.player_pos.pop(self.address, None)
             print(f"Client {self.address} est déconnecté")
-
-
 
 class GameDataSender(threading.Thread):
 
     def __init__(self, server):
         super().__init__()
-
         self.server = server
 
     def run(self):
         self.regroup_data()
 
     def regroup_data(self):
-        """Envoie les données importantes aux clients"""
+        """Envoie les données importantes aux clients."""
         while True:
-            time.sleep(0.015)  # Pour que le client puisse se connecter sans se faire deconnecter directement
+            time.sleep(0.015)
             code_and_players_pos = f"PPos, {data_base.player_pos}"
             self.server.send_data_to_clients(code_and_players_pos)
 
