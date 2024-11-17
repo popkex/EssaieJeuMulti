1b92a9dcc2ac35038316c55d432c32ecd01da622
popkex <clementbertrand52@gmail.com>
2024-11-17 16:54:23 +0100

    remise des collisions avec les joueurs

--- entity.py
+++ entity.py
@@ -57,7 +57,7 @@ class Entity:
         zone_collide = self.game.game_physic.collide(self.position, (self.scale, self.scale))
         first_corify_pos = self.rectify_position((x, y), zone_collide)
         # detecte la collisions avec les joueurs
-        zone_collide = self.game.game_physic.collide(first_corify_pos, (self.scale, self.scale))
+        zone_collide = self.game.game_physic.collide(first_corify_pos, (self.scale, self.scale), is_player=True)
         second_corify_pos = self.rectify_position(first_corify_pos, zone_collide)
 
         # Mise à jour de la position finale
